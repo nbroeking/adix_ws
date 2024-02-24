00a7ca66ed6a2e5d334501fccc92e86071d47faa
Mr. Nic <nbroeking@users.noreply.github.com>
2024-02-24 16:56:29 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Adix workforce solutions is under construction!
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
