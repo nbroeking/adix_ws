3ec8409cd4ca9b5cff42e116313bd8e291eee286
Mr. Nic <nbroeking@users.noreply.github.com>
2024-03-02 17:40:03 -0700

    Done

--- src/App.js
+++ src/App.js
@@ -24,14 +24,13 @@ function App() {
           <Heading>Adix Workforce Solutions</Heading>
           <br/>
           <br/>
-          <div classes={{height: "50px"}}></div>
           <Contributor
             fullName="Rebecca Adix"
             title="Senior Consultant"
             avatar="./headshot.jpg"
           >
             <Flex>
-              <NavLink href='mailto:rebecca@adixworkforcesolutions.com'>Email</NavLink>
+              <NavLink href='mailto:rebecca@adixworkforcesolutions.com'>Contact Me - Email</NavLink>
             </Flex>
           </Contributor>
           <ScrollDownIndicator/>
@@ -39,27 +38,42 @@ function App() {
       <Section
         bg='#EFEFEF'
         heading="Executive Experience">
-        Becky previously served as an Executive Vice President of Human Resources for the largest integrated healthcare system in the United States. Her extensive background includes occupying executive and leadership positions at various levels, ranging from hospitals and lines of business to regional and corporate settings.
-        <br/>
-        <br/>
-        With over 23 years of executive healthcare leadership experience, Becky possesses an extensive knowledge of all facets of human resources and healthcare operations. Becky is committed to providing action-based, practical solutions allowing organizations to be more effective while providing innovative strategies to meet evolving workforce needs.
+        <div style={{minHeight: "30vh"}}>
+          <div style={{paddingLeft:"10vw", paddingRight:"10vw"}}>
+            Becky previously served as an Executive Vice President of Human Resources for the largest integrated healthcare system in the United States. Her extensive background includes occupying executive and leadership positions at various levels, ranging from hospitals and lines of business to regional and corporate settings.
+          </div>
+          <br/>
+          <br/>
+          <div style={{paddingLeft:"10vw", paddingRight:"10vw"}}>
+            With over 23 years of executive healthcare leadership experience, Becky possesses an extensive knowledge of all facets of human resources and healthcare operations. Becky is committed to providing action-based, practical solutions allowing organizations to be more effective while providing innovative strategies to meet evolving workforce needs.
+          </div>
+        </div>
+
       </Section>
       <br/>
       <br/>
-      <Heading textAlign="center">Services Offered</Heading>
-      <Flex flexWrap="wrap" justifyContent="center">
-        <Feature icon="" description="I do a great job at accessing">
-          Access
-        </Feature>
-        <Feature icon="🔥" description="What your app can do">
-          Features
-        </Feature>
-        <Feature icon="📩" description="How to keep in touch">
-          Sign Up
-        </Feature>
-      </Flex>
-      <Flex is="footer" alignItems="center" p={3}>
-        <Small color="grey" ml="auto">© Adix Workforce Solutions, 2024</Small>
+      <Section
+        bg='white'
+        heading="Services Offered">
+          <div style={{minHeight: "30vh"}}>
+          <Flex flexWrap="wrap" justifyContent="center" h="100%">
+            <Feature icon="📋" description="By leveraging data-driven decision-making processes and insights gathered from comprehensive assessments,create a robust and flexible framework to optimize the delivery of HR services for maximum effectiveness and quality">
+              Assess
+            </Feature>
+            <Feature icon="🧩" description="Design and creation of an HR Strategic Roadmap allowing organizations to achieve greater value on their talent investment through an aligned, executable and integrated enterprise strategy">
+              Align
+            </Feature>
+            <Feature icon="⛔️" description="Develop a clear and adaptable framework to effectively manage and mitigate labor risks, improve communications and reduce conflicts between leadership and staff">
+              Avoid
+            </Feature>
+            <Feature icon="📩" description="Implement a tiered accountability framework extending from executive leadership to front-line supervisors, promote the adoption of practices to significantly increase pipeline for key roles, boost employee engagement and support retention">
+              Accelerate
+            </Feature>
+          </Flex>
+          </div>
+      </Section>
+      <Flex alignItems="center" p={3} bg={'#EFEFEF'}>
+        <Small color="grey" ml="auto" mr="auto">© Adix Workforce Solutions, 2024</Small>
       </Flex>
     </Provider>
   );
